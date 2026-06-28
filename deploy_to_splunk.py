fd4408bded59c8b77152283c44490796c932c31d
imanovcesur007-hue <imanovcesur007@gmail.com>
2026-06-28 21:07:58 +0400

    Update deploy_to_splunk.py

--- deploy_to_splunk.py
+++ deploy_to_splunk.py
@@ -2,14 +2,14 @@ import os
 import json
 import requests
 import urllib3
-import urllib.parse 
+import urllib.parse
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # --- SPLUNK KONFİQURASİYASI (YENİ SERVER) ---
-SPLUNK_HOST = "https://9.223.115.161:8089" 
+SPLUNK_HOST = "https://9.223.115.161:8089"
 SPLUNK_TOKEN = "eyJraWQiOiJzcGx1bmsuc2VjcmV0IiwiYWxnIjoiSFM1MTIiLCJ2ZXIiOiJ2MiIsInR0eXAiOiJzdGF0aWMifQ.eyJpc3MiOiJtaWxsaXNlYyBmcm9tIHNwbHVua3NlcnZlciIsInN1YiI6Im1pbGxpc2VjIiwiYXVkIjoiR2l0aHViX2FwaSIsImlkcCI6IlNwbHVuayIsImp0aSI6Ijc2ZTkzZWM4M2Q3MGVhOGE1ZTU0MTRmOWI4YTQ1OWIwMTEwM2U4MGJkN2RlNGRjNmNiZmVlNmM4MjgxN2Q1NzkiLCJpYXQiOjE3ODIyMTc1NzAsImV4cCI6MTc4NDgwOTU3MCwibmJyIjoxNzgyMjE3NTcwfQ.fqp4Kcv9nuy_XSl0IyS6QcklHvgf17fOhPT4uOYp2P3cqPYo6VXRi_aobA3u15nUZs6KIMg517T0s-5yKN-wPw"
-APP_CONTEXT = "search" 
+APP_CONTEXT = "search"
 API_ENDPOINT = f"{SPLUNK_HOST}/servicesNS/nobody/{APP_CONTEXT}/saved/searches"
 
 # --- TELEGRAM KONFİQURASİYASI ---
@@ -26,13 +26,13 @@ RULES_DIR = "rules/splunk/"
 def deploy_rule_to_splunk(rule_data):
     rule_name = rule_data["rule_name"]
     
-    telegram_message = f"🚨 SOC ALERT: YENİ TƏHDİD AŞKARLANDI! 🚨\n\nQayda: {rule_name}\nKateqoriya: {rule_data['owasp_category']}\nCiddiyyət: {rule_data['severity']}\n\nTəcili Splunk panelinə daxil olub analiz edin!"
+    telegram_message = f"🚨 SOC ALERT: YENİ TƏHDİD AŞKARLANDI! 🚨\n\nQayda: {rule_name}\nKateqoriya: {rule_data['owasp_id']}:{rule_data['owasp_version']}-{rule_data['owasp_name']}\nCiddiyyət: {rule_data['severity']}\n\nTəcili Splunk panelinə daxil olub analiz edin!"
     encoded_message = urllib.parse.quote(telegram_message)
     TELEGRAM_WEBHOOK_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage?chat_id={TELEGRAM_CHAT_ID}&text={encoded_message}"
     
     payload = {
         "search": rule_data["search_query"],
-        "description": f"{rule_data['description']} | OWASP Category: {rule_data['owasp_category']} | Severity: {rule_data['severity']}",
+        "description": f"{rule_data['description']} | OWASP Category: {rule_data['owasp_id']}:{rule_data['owasp_version']}-{rule_data['owasp_name']} | Severity: {rule_data['severity']}",
         "disabled": "0",
         "is_scheduled": "1",
         "cron_schedule": "* * * * *", 
